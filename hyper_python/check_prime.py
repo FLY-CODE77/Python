591dd434562282742f4b9fd927d933efb381ef5c
FLY-CODE77 <lightyear94122@gmail.com>
2021-05-27 17:25:48 +0900

    add Docs

--- hyper_python/check_prime.py
+++ hyper_python/check_prime.py
@@ -1,5 +1,14 @@
 import math
 
+'''
+prime number is only decompostion by 1 * self_num
+it can be use all data to define prime or not 
+But time o is o(n)
+
+to speed up
+use sqrt(square root) 
+time o(root(n))
+'''
 def check_prime(number):
     sqrt_number = math.sqrt(number)
     for i in range(2, int(sqrt_number) + 1):
